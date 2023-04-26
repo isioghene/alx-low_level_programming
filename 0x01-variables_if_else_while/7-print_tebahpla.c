577add0ef52737e19639494ced9090192509ecc8
isioghene <giftedisio@gmail.com>
2023-04-26 08:21:50 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Description: prints alphabets in reverse order
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
